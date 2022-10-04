210b2690de3a9dc7873e2a4a89a315589be75546
Carina Calixto Ribeiro de Araujo <carinacalixto@gmail.com>
2022-10-04 20:05:52 -0300

    Gerente herda de Funcionario

--- Python/oo/gerente.py
+++ Python/oo/gerente.py
@@ -1,12 +1,13 @@
 # Criando uma classe Gerente que possua os mesmos métodos e atributos de Funcionário, 
 # porém com informações adicionais.
 
-class Gerente:
+from Python.oo.funcionario import Funcionario
+
+class Gerente(Funcionario):
     
     def __init__(self, nome, cpf, salario, senha, qtd_gerenciados) -> None:
-        self._nome = nome
-        self._cpf = cpf
-        self._salario = salario
+        # Funcionario.__init__(nome, cpf, salario)
+        super().__init__(nome, cpf, salario)
         self._senha = senha
         self._qtd_gerenciados = qtd_gerenciados
     
